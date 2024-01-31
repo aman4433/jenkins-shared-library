8a9ddf3ec1e9c986cc337d7b832aec3f9f2e2427
Aman Shrivastava <amanshrivastava118@gmail.com>
2024-01-31 15:51:56 +0530

    reverting changes

--- vars/BuildPipeline.groovy
+++ vars/BuildPipeline.groovy
@@ -1,63 +1,58 @@
-// pipeline.groovy
 
-import org.jenkinsci.plugins.workflow.steps.StepContext
-import org.jenkinsci.plugins.workflow.steps.StepExecution
-
-class Pipeline implements Serializable {
-    @DataBoundConstructor
-    Pipeline() {}
-
-    def call(StepContext context) {
-        pipelineStages()
-    }
-
-    def pipelineStages() {
-        checkoutAndBuild()
-        owaspFsScan()
-        dockerBuild()
-    }
-
-    def checkoutAndBuild() {
-        stage('checkout and build') {
-            agent {
-                docker {
-                    image 'maven:3.8.1-adoptopenjdk-11'
-                    args '-v $HOME/.m2:/root/.m2'
-                }
-            }
-            steps {
-                script {
-                    checkout([$class: 'GitSCM',
-                               branches: [[name: '*/main']],
-                               userRemoteConfigs: [[url: "https://$GITHUB_TOKEN@github.com/aman4433/spring-boot-hello-world.git"]]])
-                }
-                sh 'pwd'
-                sh 'ls -ltr'
-                sh 'mvn --version'
-                sh 'mvn clean install'
-            }
+def call() {
+    pipeline {
+        agent any
+        environment {
+            GITHUB_TOKEN = credentials('GITPAT')
+        }
+        stages {
+            checkoutAndBuild()
+            owaspFsScan()
+            dockerBuild()
         }
     }
+}
 
-    def owaspFsScan() {
-        stage('OWASP FS SCAN') {
-            steps {
-                dependencyCheck additionalArguments: '--scan ./ --disableYarnAudit --disableNodeAudit', odcInstallation: 'Dependency-Check'
-                dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+def checkoutAndBuild() {
+    stage('checkout and build') {
+        agent {
+            docker {
+                image 'maven:3.8.1-adoptopenjdk-11'
+                args '-v $HOME/.m2:/root/.m2'
             }
         }
+        steps {
+            script {
+                checkout([$class: 'GitSCM',
+                           branches: [[name: '*/main']],
+                           userRemoteConfigs: [[url: "https://$GITHUB_TOKEN@github.com/aman4433/spring-boot-hello-world.git"]]])
+            }
+            sh 'pwd'
+            sh 'ls -ltr'
+            sh 'mvn --version'
+            sh 'mvn clean install'
+        }
     }
+}
 
-    def dockerBuild() {
-        stage('docker build') {
-            agent any
-            steps {
-                script {
-                    // Access Git commit ID using GIT_COMMIT environment variable
-                    def gitCommitId = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
-                    echo "Git Commit ID: ${gitCommitId}"
-                    sh "docker build -t hello-world:${gitCommitId} ."
-                }
+def owaspFsScan() {
+    stage('OWASP FS SCAN') {
+        steps {
+            dependencyCheck additionalArguments: '--scan ./ --disableYarnAudit --disableNodeAudit', odcInstallation: 'Dependency-Check'
+            dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+        }
+    }
+}
+
+def dockerBuild() {
+    stage('docker build') {
+        agent any
+        steps {
+            script {
+                // Access Git commit ID using GIT_COMMIT environment variable
+                def gitCommitId = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+                echo "Git Commit ID: ${gitCommitId}"
+                sh "docker build -t hello-world:${gitCommitId} ."
             }
         }
     }
