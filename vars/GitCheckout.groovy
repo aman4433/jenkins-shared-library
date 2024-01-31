f6b09d82421328cae48e84f2ffc34cd895543284
Aman Shrivastava <amanshrivastava118@gmail.com>
2024-01-31 16:23:47 +0530

    adding build stage

--- vars/GitCheckout.groovy
+++ vars/GitCheckout.groovy
@@ -9,12 +9,22 @@ def call() {
         }
 
         stages {
-            stage('Git Checkout') {
+            stage('checkout and build') {
+                agent {
+                    docker {
+                        image 'maven:3.8.1-adoptopenjdk-11'
+                        args '-v $HOME/.m2:/root/.m2'
+                    }
+                }
                 steps {
                     script {
                         checkout([$class: 'GitSCM', 
                                   branches: [[name: '*/main']], 
                                   userRemoteConfigs: [[url: "https://$GITHUB_TOKEN@github.com/aman4433/spring-boot-hello-world.git"]]])
+                        sh 'pwd'
+                        sh 'ls -ltr'
+                        sh 'mvn --version'
+                        sh 'mvn clean install'
                     }
                 }
             }
